484a6aaaf484dd448e9384aba1a728509a7b4bbb
JavierBurgara <135621096+JavierBurgara@users.noreply.github.com>
2023-07-24 21:02:20 -0700

    added context to my function

--- assets/script.js
+++ assets/script.js
@@ -2,5 +2,8 @@
 var searchHistory = [];
 // returns local storage search history
 function getItems() {
-    
+    var storedCities = JSON.parse(localStorage.getItem("searchHistory"));
+    if(storedCities !== null) {
+        searchHistory = storedCities;
+    }
 }
