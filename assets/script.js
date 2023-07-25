e93d3e8fb918bd369370f142ebfbd553394f0076
JavierBurgara <135621096+JavierBurgara@users.noreply.github.com>
2023-07-24 21:50:06 -0700

    added a for loop to getItems function

--- assets/script.js
+++ assets/script.js
@@ -5,5 +5,20 @@ function getItems() {
     var storedCities = JSON.parse(localStorage.getItem("searchHistory"));
     if(storedCities !== null) {
         searchHistory = storedCities;
-    }
-}
+    };
+
+    // lists up to 10 locations
+for (i = 0; i < searchHistory.length; i++) {
+    if (i == 10) {
+        break;
+      }
+    //  creates links/buttons https://getbootstrap.com/docs/4./components/list-group/
+    cityListButton = $("<a>").attr({
+        class: "list-group-item list-group-item-action",
+        href: "#"
+    });
+    // appends history as a button below the search field
+    cityListButton.text(searchHistory[i]);
+    $(".list-group").append(cityListButton);
+        }
+    };
