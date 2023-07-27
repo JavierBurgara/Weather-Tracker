917cebd769fdd2effce56c8c1889c4f9651bed31
JavierBurgara <135621096+JavierBurgara@users.noreply.github.com>
2023-07-26 23:18:40 -0700

    added readme

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # Weather-Tracker
+
+# Description
+
+# Installation
+HTML,CSS,JavaScript
+# Usage
+
+# Credits
+
+# License
+
+# Link to Website
+https://javierburgara.github.io/Weather-Tracker/
+
+# Deployed image of Website
+![Module 6](https://github.com/JavierBurgara/Weather-Tracker/assets/135621096/9449c2f2-67fe-48c1-9636-2d6b9924a93a)
--- assets/script.js
+++ assets/script.js
@@ -1,79 +1,86 @@
-// list (empty until given event)
+// contains list (empty until given event)
 var searchHistory = [];
 // returns local storage search history
 function getItems() {
     var storedCities = JSON.parse(localStorage.getItem("searchHistory"));
-    if(storedCities !== null) {
+    if (storedCities !== null) {
         searchHistory = storedCities;
     };
-
-    // lists up to 10 locations
-for (i = 0; i < searchHistory.length; i++) {
-    if (i == 10) {
-        break;
-      }
-    //  creates links/buttons https://getbootstrap.com/docs/4./components/list-group/
-    cityListButton = $("<a>").attr({
-        class: "list-group-item list-group-item-action",
-        href: "#"
-    });
-    // appends history as a button below the search field
-    cityListButton.text(searchHistory[i]);
-    $(".list-group").append(cityListButton);
-        }
-    };
-    
-    var city;
-    var mainCard = $(".card-body");
-    // prompt getItems
-    getItems();
-    // main card
-    function getData() {
-        // my api code
-        var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=8af713bebedab909ba40e0054699acdb" // call for city weather conditions
-        mainCard.empty();
-        $("#weeklyForecast").empty();
-        // requests
+     // lists up to 8 locations
+    for (i = 0; i < searchHistory.length; i++) {
+        if (i == 8) {
+            break;
+          }
+        //  creates links/buttons https://getbootstrap.com/docs/4./components/list-group/
+        cityListButton = $("<a>").attr({
+            class: "list-group-item list-group-item-action",
+            href: "#"
+        });
+        // appends history as a button below the search field
+        cityListButton.text(searchHistory[i]);
+        $(".list-group").append(cityListButton);
+    }
+};
+var city;
+var mainCard = $(".card-body");
+// prompt getItems
+getItems();
+// main card
+function getData() { // my api code
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=8af713bebedab909ba40e0054699acdb" // starts call for current conditions
+    mainCard.empty();
+    $("#weeklyForecast").empty();
+    // requests
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        // using moment to call the date
+        var date = moment().format(" MM/DD/YYYY");
+        // takes the icon code from the response and assigns it to iconCode
+        var iconCode = response.weather[0].icon;
+        // builds the main card icon url
+        var iconURL = "http://openweathermap.org/img/w/" + iconCode + ".png";
+        // takes the name added from the search and the date/format from moment and creates a single var
+        var name = $("<h3>").html(city + date);
+        // displays name in main card
+        mainCard.prepend(name);
+        // displays icon on main card
+        mainCard.append($("<img>").attr("src", iconURL));
+        // converts K and removes decimals using Math.round
+        var temp = Math.round((response.main.temp - 273.15) * 1.80 + 32);
+        mainCard.append($("<p>").html("Temperature: " + temp + " &#8457")); //appends fahrenheit degrees using short key code
+        var humidity = response.main.humidity;
+        mainCard.append($("<p>").html("Humidity: " + humidity)); // appends humidity
+        var windSpeed = response.wind.speed;
+        mainCard.append($("<p>").html("Wind Speed: " + windSpeed)); // appends windspeed
+        // takes from the response and creates a var used in the next request for UV index
+        var lat = response.coord.lat;
+        var lon = response.coord.lon;
+        // separate request for UV index, requires lat/long
         $.ajax({
-            url: queryURL,
+            url: "https://api.openweathermap.org/data/2.5/uvi?appid=8af713bebedab909ba40e0054699acdb&lat=" + lat + "&lon=" + lon, // my api code
             method: "GET"
-        }).then(function(response){
-            var date = moment().format("MM/DD/YYYY");
-            var iconCode = response.weather[0].icon;
-            var iconURL = "http://openweathermap.org/img/w/" + iconCode + ".png";
-            var name = $("<h3>").html(city + date);
-            mainCard.prepend(name);
-            mainCard.append($("<img>").attr("src", iconURL));
-            var temp = Math.round((response.main.temp - 273.15) *1.80 + 32);
-            mainCard.append($("<p>").html("Temperature: " + temp + " &#8457")); 
-            var humidity = response.main.humidity;
-            mainCard.append($("<p>").html("Humidity: " + humidity));
-            var windSpeed = response.wind.speed;
-            mainCard.append($("<p>").html("Wind Speed: " + windSpeed)); 
-            var lat = response.coord.lat;
-            var lon = response.coord.lon;
-            $.ajax ({
-                url: "https://api.openweathermap.org/data/2.5/uvi?appid=8af713bebedab909ba40e0054699acdb=" + lat + "&lon=" + lon, // my api code
-                method: "GET"
-            }).then(function(response) {
-                mainCard.append($("<p>").html("UV Index: <span>" + response.value + "</span>"));
-
-                if (response.value <= 2){
-                    $("span").attr("class", "btn btn-outline-success");
-                };
-                if (response.value > 2 && response.value <= 5) {
-                    $("span").attr("class", "btn btn-outline-warning");
-                };
-                if (response.value > 5) {
-                    $("span").attr("class", "btn btn-outline-danger");
-                };
-            });
-            // calls for 5 day forecast
-            $.ajax({
-                url: "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=8af713bebedab909ba40e0054699acdb", // my api code
-                method: "GET"
-
-            }).then(function (response) {
+        // displays UV in main card and is color coded based off of intensity
+        }).then(function (response) {
+            mainCard.append($("<p>").html("UV Index: <span>" + response.value + "</span>"));
+            // 
+            if (response.value <= 2) {
+                $("span").attr("class", "btn btn-outline-success");
+            };
+            if (response.value > 2 && response.value <= 5) {
+                $("span").attr("class", "btn btn-outline-warning");
+            };
+            if (response.value > 5) {
+                $("span").attr("class", "btn btn-outline-danger");
+            };
+        })
+        // another call for the 5-day (forecast)
+        $.ajax({
+            url: "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=8af713bebedab909ba40e0054699acdb", // my api code
+            method: "GET"
+        // displays 5 separate columns from the forecast response for 5 days
+        }).then(function (response) {
             for (i = 0; i < 5; i++) { // start for loop
                 // creates the columns
                 var newCard = $("<div>").attr("class", "col fiveDay bg-primary text-white rounded-lg p-2");
@@ -96,11 +103,12 @@ for (i = 0; i < searchHistory.length; i++) {
                 var humidity = response.list[i * 8].main.humidity;
                 // displays humidity
                 newCard.append($("<p>").html("Humidity: " + humidity));
-                 }
-             })
+            }
         })
-    };
-    $("#searchCity").click(function() {
+    })
+};
+// searches and adds to history(event)
+$("#searchCity").click(function() {
     city = $("#city").val().trim();
     getData();
     var checkArray = searchHistory.includes(city);
@@ -137,4 +145,3 @@ $('#clear').click( function() {
     location.reload();
     return false;
     });
-    
