2c45777d96fb6eb5a500a295553ea97749941b49
JavierBurgara <135621096+JavierBurgara@users.noreply.github.com>
2023-07-26 19:07:47 -0700

    added listen and actions for history button and search city and list group items

--- assets/script.js
+++ assets/script.js
@@ -100,5 +100,41 @@ for (i = 0; i < searchHistory.length; i++) {
              })
         })
     };
-    
+    $("#searchCity").click(function() {
+    city = $("#city").val().trim();
+    getData();
+    var checkArray = searchHistory.includes(city);
+    if (checkArray == true) {
+        return
+    }
+    else {
+        searchHistory.push(city);
+        localStorage.setItem("searchHistory", JSON.stringify(searchHistory));
+        var cityListButton = $("<a>").attr({
+            // list-group-item-action keeps the search history buttons consistent
+            class: "list-group-item list-group-item-action",
+            href: "#"
+        });
+        cityListButton.text(city);
+        $(".list-group").append(cityListButton);
+    };
+});
+// listens for action on the history buttons(event)
+$(".list-group-item").click(function() {
+    city = $(this).text();
+    getData();
+});
+// capitalize city name
+$("#searchCity").keypress(function () {  
+    var _val = $("#searchCity").val();  
+    var _txt = _val.charAt(0).toUpperCase() + _val.slice(1);  
+    $("#searchCity").val(_txt);
+});
+
+// clear all the local storage(not working after a page refresh)
+$('#clear').click( function() {
+    window.localStorage.clear();
+    location.reload();
+    return false;
+    });
     
