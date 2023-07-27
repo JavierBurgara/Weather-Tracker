a342b06c713cf1ffdae9ed1990d194f6e3f0846e
JavierBurgara <135621096+JavierBurgara@users.noreply.github.com>
2023-07-26 17:19:47 -0700

    added for loop

--- assets/script.js
+++ assets/script.js
@@ -37,15 +37,36 @@ for (i = 0; i < searchHistory.length; i++) {
         $.ajax({
             url: queryURL,
             method: "GET"
-        }).then(function(reponse){
+        }).then(function(response){
             var date = moment().format("MM/DD/YYYY");
-            var iconCode = reponse.weather[0].icon;
+            var iconCode = response.weather[0].icon;
             var iconURL = "http://openweathermap.org/img/w/" + iconCode + ".png";
             var name = $("<h3>").html(city + date);
             mainCard.prepend(name);
             mainCard.append($("<img>").attr("src", iconURL));
-            var temp = Math.round((reponse.main.temp - 273.15) *1.80 + 32);
-            
+            var temp = Math.round((response.main.temp - 273.15) *1.80 + 32);
+            mainCard.append($("<p>").html("Temperature: " + temp + " &#8457")); 
+            var humidity = response.main.humidity;
+            mainCard.append($("<p>").html("Humidity: " + humidity));
+            var windSpeed = response.wind.speed;
+            mainCard.append($("<p>").html("Wind Speed: " + windSpeed)); 
+            var lat = response.coord.lat;
+            var lon = response.coord.lon;
+            $.ajax ({
+                url: "https://api.openweathermap.org/data/2.5/uvi?appid=8af713bebedab909ba40e0054699acdb=" + lat + "&lon=" + lon, // my api code
+                method: "GET"
+            }).then(function(response) {
+                mainCard.append($("<p>").html("UV Index: <span>" + response.value + "</span>"));
 
+                if (response.value <= 2){
+                    $("span").attr("class", "btn btn-outline-success");
+                };
+                if (response.value > 2 && response.value <= 5) {
+                    $("span").attr("class", "btn btn-outline-warning");
+                };
+                if (response.value > 5) {
+                    $("span").attr("class", "btn btn-outline-danger");
+                };
+            });
         })
     };
